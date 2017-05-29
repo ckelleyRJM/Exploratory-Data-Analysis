0603e374c7107f8dd0defcb23651b9c9da3fce2c
Craig Kelley <kelley.cw@gmail.com>
2017-05-28 21:11:11 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,4 +1,7 @@
 # Read data file
+Download the file manually from this link: https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+Set your working directory as wherever that ^ file is, or move the file to your current working directory
+
 hpcdata <- "./household_power_consumption.txt"
 data <- read.table(hpcdata, header = TRUE, sep = ";", stringsAsFactors = FALSE, dec = ".")
 
